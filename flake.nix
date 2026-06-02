c9da457afee928e12328a64782297431492dc2c1
pwnwriter <hi@pwnwriter.me>
2026-06-01 23:14:33 -0400

    init(skel): skeleton for the app w nix

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems =
+        function: nixpkgs.lib.genAttrs systems (system: function (import nixpkgs { inherit system; }));
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+
+          packages = with pkgs; [
+            # uv
+            # python314
+          ];
+
+          shellHook = ''
+            echo "------eipi.boo--------"
+          '';
+
+          env = {
+            # Linux
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+              pkgs.stdenv.cc.cc.lib
+            ];
+            # MacOS
+            DYLD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+              pkgs.stdenv.cc.cc.lib
+            ];
+          };
+        };
+      });
+    };
+}
